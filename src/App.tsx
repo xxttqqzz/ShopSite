9a460f52f86f8b9edffadb8f19390bf9d22cbc9d
Jyystaja <iisak.merilehto@gmail.com>
2025-05-16 21:12:44 +0300

    reactrouterrom final location

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "../node_modules/react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
